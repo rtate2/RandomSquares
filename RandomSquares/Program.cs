18817536c4e011c4fc0779e8a5d8a20c983c99f9
Randy Tate <randytatejr@gmail.com>
2020-03-20 21:24:27 -0500

    Add project files.

---
+++ RandomSquares/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RandomSquares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            // Create a list
+            List<int> listOfNumbers = new List<int> { };
+
+            // Populate the random numbers list
+            for (int ctr = 0; ctr <= 19; ctr++)
+                listOfNumbers.Add(random.Next(0, 51));
+
+            // Create squared numbers list
+            List<int> squaredNumbersList = new List<int> { };
+
+            // Populate the squared numbers list
+            foreach (var randomNumber in listOfNumbers)
+            {
+                squaredNumbersList.Add(randomNumber * randomNumber);
+            }
+
+            // Removing all odd numbers from the list
+            squaredNumbersList.RemoveAll(i => i % 2 != 0);
+        }
+    }
+}
